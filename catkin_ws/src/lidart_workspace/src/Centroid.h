df0b4755ebe899dce1e0ca4b4588d7aeff581bf4
jjsa99 <joaoalegria1999@hotmail.com>
2021-05-27 10:31:12 +0100

    test commit

--- catkin_ws/src/lidart_workspace/src/Centroid.h
+++
@@ -1,7 +0,0 @@
-/**
- * \brief Find Centroid function - It scans throught the images and finds where the max intensity of light is. It can detect if it's noise or part of the centroid.
- * @param [in] data: data pointer
- * @param [in] centroidCoord : array with the coordinates of the centroid.
- */
-
-int findCentroid(uint8_t *data, double centroidCoord[]);
